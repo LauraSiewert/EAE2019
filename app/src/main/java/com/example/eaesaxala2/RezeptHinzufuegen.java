81487ca0a81d4652752838ea2956d3ab7c713aff
Laura <lasi0002@stud.hs-kl.de>
2019-04-03 16:58:02 +0200

    -Suche verbessert

--- app/src/main/java/com/example/eaesaxala2/RezeptHinzufuegen.java
+++ app/src/main/java/com/example/eaesaxala2/RezeptHinzufuegen.java
@@ -127,8 +127,75 @@ public class RezeptHinzufuegen extends AppCompatActivity implements View.OnClick
 
         if(requestCode == CAM_REQUEST){
             bitmap = (Bitmap) data.getExtras().get("data");
+            Log.d("SL", "extras ist: "+ bitmap);
             foto.setImageBitmap(bitmap);
         }
+    }
+
+    //@Override
+    /*protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            /*Bundle extras = data.getExtras();
+            Log.d("SL", "extras ist: "+extras);
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            bitmap = (Bitmap) data.getExtras().get("data");
+            foto.setImageBitmap(bitmap);
+        }
+    }*/
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String currentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                Log.d("SL", "Das File wurde kreiert");
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+                //Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //startActivityForResult(intent,CAM_REQUEST);
+            }
+        }
+    }
+
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
     }
 
 
@@ -192,8 +259,9 @@ public class RezeptHinzufuegen extends AppCompatActivity implements View.OnClick
             startActivity(detail_viewInt);
         }
         else if(v==fotob){
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(intent,CAM_REQUEST);
+            dispatchTakePictureIntent();
+            //Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            //startActivityForResult(intent,CAM_REQUEST);
         }
         else if (v==zutatenb){
             AlertDialog.Builder dialog = new AlertDialog.Builder(this);
