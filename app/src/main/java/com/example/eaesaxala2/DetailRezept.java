37a90fe765dde27a4c4894eedb52c42f1b168aa5
Xaver Lutz <xalu0001@stud.hs-kl.de>
2019-05-06 17:42:48 +0200

    Detail View, mit löschen, noch ohne Bearbeiten Funktion

--- app/src/main/java/com/example/eaesaxala2/DetailRezept.java
+++ app/src/main/java/com/example/eaesaxala2/DetailRezept.java
@@ -8,10 +8,16 @@ import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.database.DatabaseUtils;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -28,6 +34,18 @@ public class DetailRezept extends AppCompatActivity {
 
         db = new DatenBankManager(this);
 
+        //get View Elemente
+        TextView name = findViewById(R.id.NAMEDETAIL);
+        ImageView foto = findViewById(R.id.FOTODETAIl);
+        TextView zeit = findViewById(R.id.ZEITDETAIL);
+        SeekBar schwierigkeit = findViewById(R.id.SCHWIERIGKEITSDETAIL);
+        ImageView hauptkategorie = findViewById(R.id.HAUPTKATEGORIEDETAIL);
+        ImageView unterkategorie = findViewById(R.id.UNTERKATEGORIEDETAIL);
+        TextView vorgehensweise = findViewById(R.id.VORGEHENSWEISE_EDITTEXT);
+        ListView zutatenListe = (ListView) findViewById(R.id.ZUTATEN_LISTE_DETAIL);
+
+        registerForContextMenu(name);
+
         //Navigationbar ändern
         setTitle("Rezept Details");
 
@@ -38,30 +56,75 @@ public class DetailRezept extends AppCompatActivity {
         rezeptId.setText(id);
 
         if (!(id.equals(null))){
-            TextView name = findViewById(R.id.NAMEDETAIL);
+
             Rezept rezept =  db.selectRezept(id);
             name.setText(rezept.getName());
-            ImageView foto = findViewById(R.id.FOTODETAIl);
+
             Bitmap bitmap = BitmapFactory.decodeFile(rezept.foto);
             foto.setImageBitmap(bitmap);
-            TextView zeit = findViewById(R.id.ZEITDETAIL);
+
             zeit.setText(Integer.toString(rezept.getZeit()));
-            TextView bewertung = findViewById(R.id.BEWERTUNGDETAIL);
-            bewertung.setText(Integer.toString(rezept.getBewertung()));
-            TextView schwierigkeitsgrad = findViewById(R.id.SCHWIERIGKEITSGRADDETAIL);
-            schwierigkeitsgrad.setText(Integer.toString(rezept.getSchwierigkeitsgrad()));
-            TextView hauptkategorie = findViewById(R.id.HAUPTKATEGORIEDETAIL);
-            hauptkategorie.setText(rezept.hauptkategorie);
-            TextView unterkategorie = findViewById(R.id.UNTERKATEGORIEDETAIL);
-            unterkategorie.setText(rezept.unterkategorie);
+            Log.d("XL","Bewertung: " + Integer.toString(rezept.getBewertung()));
+
+            Log.d("XL", "Schwierigkeit: " + Integer.toString(rezept.getSchwierigkeitsgrad()));
+            schwierigkeit.setProgress(rezept.getSchwierigkeitsgrad());
+
+            if ((rezept.hauptkategorie).equals("Backen")){
+                hauptkategorie.setImageResource(R.drawable.cupcake);
+            } else if ((rezept.hauptkategorie).equals("Kochen")){
+                hauptkategorie.setImageResource(R.drawable.cooking);
+            }
+
+            if ((rezept.unterkategorie).equals("vegan")){
+                unterkategorie.setImageResource(R.drawable.leaves);
+            } else if ((rezept.unterkategorie).equals("vegetarisch")){
+                unterkategorie.setImageResource(R.drawable.broccoli);
+            } else {
+                unterkategorie.setImageResource(R.drawable.meat);
+            }
+
             Log.d("SL", "Das ist die Vorgehensweise: "+ rezept.vorgehensweise);
-            TextView vorgehensweise = findViewById(R.id.VORGEHENSWEISE_EDITTEXT);
+
             vorgehensweise.setText(rezept.vorgehensweise);
-            ListView zutatenListe = (ListView) findViewById(R.id.ZUTATEN_LISTE_DETAIL);
+
             ArrayList <Zutaten> zutaten = rezept.getZutaten();
             Log.d("SL", "das sind die zutaten" + zutaten.toString());
             ZutatenAdapter mAdapter = new ZutatenAdapter(ctx, zutaten);
             zutatenListe.setAdapter(mAdapter);
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.delete_edit_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.ITEM_EDIT:
+                Toast.makeText(this,"EDIT SELECTED", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.ITEM_DELETE2:
+                Intent i = getIntent();
+                String id =  i.getStringExtra("id");
+                if (id != null) {
+                    int id_zahl = Integer.parseInt(id);
+                    db.deleteRezept(id_zahl);
+                    Intent mainActivity = new Intent(this, MainActivity.class);
+                    startActivity(mainActivity);
+                    Toast.makeText(this,"gelöscht", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                Toast.makeText(this,"UPS", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
 }
